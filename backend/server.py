53fabd6fa9594ab8dba6948dd78e689cc24f67a7
Keegan Smith <keeganasmith2003@tamu.edu>
2023-11-11 15:04:27 -0600

    server

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 from flask import request, jsonify
 import json
 import pandas as pd
@@ -23,11 +24,28 @@ def predict_classes(list_of_one_hot):
         result.append(class_result)
     return result
 clf = getModel()
+def convert_dict_values_to_int(input_dict):
+    # Create a new dictionary to store the converted values
+    result_dict = {}
+
+    for key, value in input_dict.items():
+        try:
+            # Attempt to convert the value to an integer
+            int_value = int(value)
+            result_dict[key] = int_value
+        except (ValueError, TypeError):
+            # If conversion fails, keep the original value
+            result_dict[key] = value
+
+    return result_dict
 @app.route('/api/getResult', methods=['POST'])
 def your_post_handler():
     data =request.json  # Get the JSON data from the request
     # Process the data as needed
+    print(data)
+    data = convert_dict_values_to_int(data)
     dataframe = pd.DataFrame([data])
+    
     categorical_cols = ["sex", "Pstatus", "address", "famsize","Mjob", "Fjob","reason", "guardian", "schoolsup", "famsup", "paid", "activities", "nursery", "higher", "internet", "romantic"]
     for cat in categorical_cols:
         dataframe[cat] = dataframe[cat].astype("category")
